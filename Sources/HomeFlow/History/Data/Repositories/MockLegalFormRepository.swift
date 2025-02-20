d9151fe9055ddeaba538ebcb5e91812ff6612d57
Ilham <guitardio90@gmail.com>
2025-02-20 17:20:22 +0700

    create row view of ask legal form

--- Sources/HomeFlow/History/Data/Repositories/MockLegalFormRepository.swift
+++ Sources/HomeFlow/History/Data/Repositories/MockLegalFormRepository.swift
@@ -14,30 +14,30 @@ public class MockLegalFormRepository: LegalFormRepositoryLogic {
   
   public func fetchLegalFormDocuments() async throws -> [LegalFormDocumentEntity] {
     return [
-      LegalFormDocumentEntity(
-        type: "ACTIVE",
-        status: "WAITING_FOR_PAYMENT",
-        title: "Perjanjian Pinjam Meminjam (Utang Piutang)",
-        timeRemaining: 0.0,
-        date: "",
-        price: "Rp10.000"
-      ),
-      LegalFormDocumentEntity(
-        type: "ACTIVE",
-        status: "ON_PROCESS",
-        title: "Surat Pernyataan Ahli Waris",
-        timeRemaining: 0.0,
-        date: "",
-        price: "Rp100.000"
-      ),
-      LegalFormDocumentEntity(
-        type: "HISTORY",
-        status: "DONE",
-        title: "Surat Pernyataan Ahli Waris",
-        timeRemaining: 0.0,
-        date: "",
-        price: "Rp100.000"
-      )
+//      LegalFormDocumentEntity(
+//        type: "ACTIVE",
+//        status: "WAITING_FOR_PAYMENT",
+//        title: "Perjanjian Pinjam Meminjam (Utang Piutang)",
+//        timeRemaining: 0.0,
+//        date: "",
+//        price: "Rp10.000"
+//      ),
+//      LegalFormDocumentEntity(
+//        type: "ACTIVE",
+//        status: "ON_PROCESS",
+//        title: "Surat Pernyataan Ahli Waris",
+//        timeRemaining: 0.0,
+//        date: "",
+//        price: "Rp100.000"
+//      ),
+//      LegalFormDocumentEntity(
+//        type: "HISTORY",
+//        status: "DONE",
+//        title: "Surat Pernyataan Ahli Waris",
+//        timeRemaining: 0.0,
+//        date: "",
+//        price: "Rp100.000"
+//      )
     ]
   }
   
--- Sources/HomeFlow/History/Presentation/Views/LegalFormView.swift
+++ Sources/HomeFlow/History/Presentation/Views/LegalFormView.swift
@@ -21,58 +21,53 @@ public struct LegalFormView: View {
       
       VStack(spacing: 0) {
         
-        VStack(alignment: .leading, spacing: 8) {
-          
-          Text("Dokumen Aktif")
-            .foregroundStyle(Color.gray900)
-            .titleLexend(size: 16)
-            .padding(.bottom, 8)
-            .padding(.leading, 16)
-            .frame(maxWidth: .infinity, alignment: .leading)
-          
-          ForEach(store.activeViewModels, id: \.id) { model in
-            activeDocument(model)
-          }
-        }
-        .frame(maxWidth: .infinity)
-        .padding(.vertical, 16)
-        .background(Color.success100)
-        .padding(.top, 65)
+        documentView()
         
-        VStack(alignment: .leading, spacing: 8) {
-          
-          HStack {
-            Text("Riwayat Dokumen")
-              .foregroundStyle(Color.gray900)
-              .titleLexend(size: 16)
-              .frame(maxWidth: .infinity, alignment: .leading)
+        Divider()
+          .background(Color.gray100)
+          .frame(maxWidth: .infinity, maxHeight: 1)
+        
+          VStack(alignment: .leading, spacing: 8) {
             
-          }
-          .padding(.bottom, 16)
-          .padding(.leading, 16)
-          
-          ForEach(store.historyViewModels, id: \.id) { model in
-            
-            if let historyModel = model as? DocumentHistoryViewModel {
-              DocumentHistoryRowView(
-                viewModel: .init(
-                  type: model.type,
-                  title: historyModel.title,
-                  status: historyModel.status,
-                  date: historyModel.dateStr(),
-                  price: historyModel.price,
-                  onNext: {
-                    historyModel.onNext()
-                  }
-                )
-              )
+            HStack {
+              Text("Riwayat Dokumen")
+                .foregroundStyle(Color.gray900)
+                .titleLexend(size: 16)
+                .frame(maxWidth: .infinity, alignment: .leading)
+              
             }
+            .padding(.bottom, 16)
+            .padding(.leading, 16)
             
+            if store.historyViewModels.isEmpty {
+              Text("Anda belum memiliki riwayat dokumen. Dokumen yang pernah akan ditampilkan di sini.")
+                .foregroundStyle(Color.gray500)
+                .captionLexend(size: 14)
+                .padding(.horizontal, 16)
+             
+            } else {
+              ForEach(store.historyViewModels, id: \.id) { model in
+                if let historyModel = model as? DocumentHistoryViewModel {
+                  DocumentHistoryRowView(
+                    viewModel: .init(
+                      type: model.type,
+                      title: historyModel.title,
+                      status: historyModel.status,
+                      date: historyModel.dateStr(),
+                      price: historyModel.price,
+                      onNext: {
+                        historyModel.onNext()
+                      }
+                    )
+                  )
+                }
+              }
+              
+            }
           }
-          
-        }
-        .frame(maxWidth: .infinity)
-        .padding(.vertical, 16)
+          .frame(maxWidth: .infinity)
+          .padding(.vertical, 16)
+      
         
       }
       .padding(.bottom, 24)
@@ -86,6 +81,62 @@ public struct LegalFormView: View {
     .ignoresSafeArea(edges: .all)
     .background(Color.gray050)
     
+  }
+  
+  @ViewBuilder
+  func documentView() -> some View {
+    if store.activeViewModels.isEmpty {
+      ZStack {
+        Image("document_bg_image", bundle: .module)
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+          .frame(maxWidth: .infinity, maxHeight: 72)
+        
+        HStack {
+          Image("document", bundle: .module)
+          
+          VStack(alignment: .leading, spacing: 4) {
+            Text("Buat Dokumen Hukum")
+              .foregroundStyle(Color.primaryInfo700)
+              .titleLexend(size: 16)
+            
+            Text("Solusi Dokumen Hukum Tanpa Ribet, Cepat, dan Terjamin Keabsahannya")
+              .foregroundStyle(Color.primaryInfo700)
+              .captionLexend(size: 12)
+              .padding(.top, 2)
+          }
+          
+          Image(systemName: "chevron.right")
+            .foregroundColor(.blue)
+        }
+        .padding(.vertical, 8)
+        .padding(.horizontal, 8)
+      }
+      .cornerRadius(10)
+      .shadow(color: Color.gray200, radius: 5)
+      .padding(.all, 16)
+      .padding(.top, 65)
+      .onTapGesture {
+        
+      }
+    } else {
+      VStack(alignment: .leading, spacing: 8) {
+        Text("Dokumen Aktif")
+          .foregroundStyle(Color.gray900)
+          .titleLexend(size: 16)
+          .padding(.bottom, 8)
+          .padding(.leading, 16)
+          .frame(maxWidth: .infinity, alignment: .leading)
+        
+        ForEach(store.activeViewModels, id: \.id) { model in
+          activeDocument(model)
+        }
+      }
+      .frame(maxWidth: .infinity)
+      .padding(.vertical, 16)
+      .background(Color.success100)
+      .padding(.top, 65)
+    }
   }
   
   @ViewBuilder
