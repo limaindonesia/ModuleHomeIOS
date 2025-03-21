2cf8b8e38fdb524c561123934eae1f716367cbd5
Ilham <guitardio90@gmail.com>
2025-03-21 13:57:53 +0700

    add detail advocate new view

---
+++ Sources/HomeFlow/Dashboard/DetailAdvocate/Presentation/View/DetailAdvocateView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailAdvocateView.swift
+//  HomeFlow
+//
+//  Created by Ilham Prabawa on 21/03/25.
+//
+
+import SwiftUI
+
+struct DetailAdvocateView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+#Preview {
+  DetailAdvocateView()
+}
