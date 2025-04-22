d18873651eb30fcb9d48ca5dbb5a3ee816bb8070
Ilham <guitardio90@gmail.com>
2025-04-22 15:14:27 +0700

    modified function name

--- Sources/HomeFlow/Dashboard/Home/Presentation/Store/HomeStore.swift
+++ Sources/HomeFlow/Dashboard/Home/Presentation/Store/HomeStore.swift
@@ -1162,7 +1162,7 @@ public class HomeStore: ObservableObject {
   }
   
   public func navigateToListPromotion() {
-    promotionListNavigator.navigateToPromotioLists()
+    promotionListNavigator.navigateToPromotion()
   }
   
   //MARK: - BottomSheet
